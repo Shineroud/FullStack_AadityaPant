0834fcc981b1f9c1aa856a0a24011b7faa64057c
Shineroud <aadipant1968@gmail.com>
2026-08-03 16:43:46 +0000

    w

--- JavaScript/First.js
+++ JavaScript/First.js
@@ -4,4 +4,21 @@ console.log(Name);
 let house = 23;
 console.log(house)
 let fullName = "Aaditya Pant";
+const Product = {
+    productName: "Ball Pen",
+    Rating: "4.5",
+    Price: "268",
+    Discount: "6"
+}
+console.log(Product)
+const profile = {
+    name: "Aaditya Pant",
+    username: "@aadityapant",
+    followers: 245000,
+    following: 94,
+    isFollow: true,
+    posts: 234,
+    verified: false
 
+}
+console.log(profile)
