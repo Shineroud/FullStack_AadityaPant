30667d2c2ae62da4601c661138dcb4a514f7ea9c
Shineroud <aadipant1968@gmail.com>
2026-08-03 17:41:33 +0000

    yes

---
+++ JavaScript/Loops.js
@@ -0,0 +1,28 @@
+Name = "Aaditya Pant"
+for (let i of Name){
+    console.log(i)
+}
+const profile = {
+    name: "Aaditya Pant",
+    username: "@aadityapant",
+    followers: 245000,
+    following: 94,
+    isFollow: true,
+    posts: 234,
+    verified: false
+
+}
+// for (let key in profile){
+//     console.log("key: ", key, "values: ", profile[key])
+// }
+// for (let k = 0;k<=100;k++){
+//     if (k%2 === 0){
+//         console.log(k)
+//     }
+// }
+let promptNum = 94;
+let Gnum = prompt ("Guess the number (0 - 100): ");
+while (Gnum != promptNum){
+   Gnum = prompt ("You entered WRONG number: Guess the right number (0 - 100): ");
+}
+console.log("Congrats, you guessed the right  number")
---
+++ JavaScript/operatorsConditionalStatements.js
@@ -0,0 +1,24 @@
+let mode = "dark"
+if (mode === "dark"){
+    console.log("true")
+}
+let age = 25;
+c = age > 18 ? "Adult":"Minor";
+console.log(c)
+{
+//alert("hello");
+let Number = prompt("Your Number");
+if (Number % 5 === 0){
+    console.log("Number is a multiple of 5")
+}
+else{
+    console.log("numener is not a multiple of 5")
+}
+}
+{
+    let score = 89
+    if (score >= 80){
+        console.log("Grade A")
+    }
+    
+}
